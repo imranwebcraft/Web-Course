47c9c8b48d23b7f2e59a44413e40cca5f644f782
imran-it1 <imranhossainsakil22@gmail.com>
2023-09-14 23:18:07 +0600

    Make course details more meaningful and make sure every course card look good and same

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -7,7 +7,7 @@ const Course = ({ course, handleCourseName }) => {
 	return (
 		// Single Course Card
 		<div className="tracking-wide">
-			<div className="bg-white rounded-lg p-4">
+			<div className="bg-white rounded-lg p-4 flex flex-col h-full">
 				{/* Course Image */}
 				<div className="mb-4">
 					<img className=" w-full" src={course_img} alt="" />
@@ -20,35 +20,37 @@ const Course = ({ course, handleCourseName }) => {
 				<div className="mb-5 text-sm text-gray-600">
 					<p>{course_details}</p>
 				</div>
-				{/* Course info */}
-				<div className="flex items-center justify-between text-base text-gray-600">
-					{/* Price Btn */}
-					<button>
-						<span className="flex items-center gap-2">
-							<span className="text-gray-800">
-								<LuDollarSign />
+				<div className=" mt-auto">
+					{/* Course info */}
+					<div className="flex items-center justify-between text-base text-gray-600 mb-2">
+						{/* Price Btn */}
+						<button>
+							<span className="flex items-center gap-2">
+								<span className="text-gray-800">
+									<LuDollarSign />
+								</span>
+								Price : {price}
 							</span>
-							Price : {price}
-						</span>
-					</button>
-					{/* Credit Btn */}
-					<button>
-						<span className="flex items-center gap-2">
-							<span className="text-gray-800 font-black">
-								<BsBook />
+						</button>
+						{/* Credit Btn */}
+						<button>
+							<span className="flex items-center gap-2">
+								<span className="text-gray-800 font-black">
+									<BsBook />
+								</span>
+								Credit : {credit}hr
 							</span>
-							Credit : {credit}hr
-						</span>
-					</button>
-				</div>
-				{/* Select Button */}
-				<div
-					className="mt-6"
-					onClick={() => handleCourseName(course, id)}
-				>
-					<button className=" py-2 rounded-md w-full bg-blue-500 text-white text-lg font-semibold hover:bg-blue-600 transition-all duration-200 ease-in">
-						Select
-					</button>
+						</button>
+					</div>
+					{/* Select Button */}
+					<div
+						className="mt-auto"
+						onClick={() => handleCourseName(course, id)}
+					>
+						<button className=" py-2 rounded-md w-full bg-blue-500 text-white text-lg font-semibold hover:bg-blue-600 transition-all duration-200 ease-in">
+							Select
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
